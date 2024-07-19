41648c9b1a0cc56ae595570f01a0ce0763f0fc4e
athabarokahfc <athabarokahfc@gmail.com>
2024-07-19 03:37:39 +0000

    create: drawer

---
+++ lib/components/header.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:fruithub/components/responsive.dart';
+import 'package:fruithub/components/HeaderWebMenu.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      // mobile view
+      if (Responsive.isMobile(context))
+        Builder(
+            builder: (context) => IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                )),
+      Image.asset('assets/images/logo.png', height: 100, width: 100),
+      const Text('FruitHub',
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple)),
+      const Spacer(),
+      if (Responsive.isDesktop(context)) HeaderWebMenu(),
+      ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple,
+              elevation: 17,
+              minimumSize: const Size(25, 44)),
+          child: const Text(
+            'Join',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+          ))
+    ]);
+  }
+}
---
+++ lib/components/header_section.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fruithub/components/header.dart';
+
+class HeaderSection extends StatelessWidget {
+  const HeaderSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.blue[200],
+        padding: const EdgeInsets.all(10),
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 1230),
+          child: Column(children: [
+            const Header()
+          ]),
+        ));
+  }
+}
